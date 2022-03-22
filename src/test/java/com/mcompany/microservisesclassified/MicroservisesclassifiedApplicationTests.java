67c5ae8b1ff0395d361a1dea67980b025f36b1d1
Dmitrii <mikheevda1@gmail.com>
2022-03-22 13:00:38 -0400

    Initial commit

---
+++ src/test/java/com/mcompany/microservisesclassified/MicroservisesclassifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcompany.microservisesclassified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservisesclassifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
